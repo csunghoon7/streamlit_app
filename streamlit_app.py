609e8af5fc41cff836558e4e455fe4f3f142471f
csunghoon7 <127923840+csunghoon7@users.noreply.github.com>
2024-02-21 10:11:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,16 @@
 import streamlit
+import pandas
+
 
 streamlit.title('고객 주문 관리 내역')
 
 streamlit.header('헤더 입니다')
 
 streamlit.text('텍스트 입니다')
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index(“Fruit”)
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)) 
+streamlit.dataframe(my_fruit_list)
 
